385d567bd2e05cff87d76d9b17a54c123aa40a65
Karthick1808 <karthickspikey@gmail.com>
2018-07-06 19:51:03 +0530

    Update updateelb.sh

--- updateelb.sh
+++ updateelb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-aws elb describe-load-balancers | grep LoadBalancerName | awk '{print $2}' | cut -d ',' -f 1 | cut -d '"' -f 2 > elb.txt
-while read LINE; do aws elb modify-load-balancer-attributes --load-balancer-name "$LINE" --load-balancer-attributes "{\"ConnectionSettings\":{\"IdleTimeout\":180}}"; done < elb.txt
+aws elb describe-load-balancers | grep LoadBalancerName | awk '{print $2}' | cut -d ',' -f 1 | cut -d '"' -f 2 > fullelb.txt
+grep "private-" fullelb.txt > editelb.txt && grep "LB-HLXDEV1APIGW-" fullelb.txt >> editelb.txt
+while read LINE; do aws elb modify-load-balancer-attributes --load-balancer-name "$LINE" --load-balancer-attributes "{\"ConnectionSettings\":{\"IdleTimeout\":180}}"; done < editelb.txt
